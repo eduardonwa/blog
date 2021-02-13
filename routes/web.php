687da70788381dd6508fd31186ed00e7c6d506c8
Eduardo Robles <eduardongua@hotmail.com>
2021-02-12 19:34:56 -0700

    auth

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ use App\Http\Controllers\ContactController;
 |
 */
 
+Auth::routes(['register' => false]);
+
 Route::get('/', function () {
     return view('welcome', [
         'posts' => App\Models\Post::where('is_approved', true)->take(3)->latest()->get(),
